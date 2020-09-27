e976be8f97fb1caf67ccff37291670f6d610ab82
Trong Tran <trtran@axon.com>
2020-09-27 15:10:04 +0700

    solution for max area histogram

--- src/LargestHistogram.java
+++ src/LargestHistogram.java
@@ -7,41 +7,27 @@ public class LargestHistogram {
         // heights.
         Stack<Integer> s = new Stack<>();
 
-        int max_area = 0; // Initialize max area
-        int tp;  // To store top of stack
-        int area_with_top; // To store area with top bar as the smallest bar
+        int max_area = 0;
+        int topOfStack;
+        int area_with_top;
 
-        // Run through all bars of given histogram
         int i = 0;
         while (i < n) {
-            // If this bar is higher than the bar on top stack, push it to stack
             if (s.empty() || hist[s.peek()] <= hist[i])
                 s.push(i++);
-
-                // If this bar is lower than top of stack, then calculate area of rectangle
-                // with stack top as the smallest (or minimum height) bar. 'i' is
-                // 'right index' for the top and element before top in stack is 'left index'
             else {
-                tp = s.peek();  // store the top index
-                s.pop();  // pop the top
-
-
-                // Calculate the area with hist[tp] stack as smallest bar
-                //System.out.println(s);
-                area_with_top = hist[tp] * (s.empty() ? i : i - s.peek() - 1);
-                //System.out.println(area_with_top + " ");
-                // update max area, if needed
+                topOfStack = s.peek();
+                s.pop();
+                area_with_top = hist[topOfStack] * (s.empty() ? i : i - s.peek() - 1);
                  if (max_area < area_with_top)
                     max_area = area_with_top;
             }
         }
 
-        // Now pop the remaining bars from stack and calculate area with every
-        // popped bar as the smallest bar
         while (s.empty() == false) {
-            tp = s.peek();
+            topOfStack = s.peek();
             s.pop();
-            area_with_top = hist[tp] * (s.empty() ? i : i - s.peek() - 1);
+            area_with_top = hist[topOfStack] * (s.empty() ? i : i - s.peek() - 1);
 
             if (max_area < area_with_top)
                 max_area = area_with_top;
